ce11e79438551415c5affe3cbf39b7765d3586ec
idkim <idkim@jsoftlab.co.kr>
2026-06-25 16:50:36 +0900

    snmpvarbind 구조체 수정

--- include/snmp_asn.h
+++ include/snmp_asn.h
@@ -4,7 +4,8 @@
 #include "asn1.h"
 #include "libcore.h"
 
-typedef struct SnmpVarBind {
+typedef  struct SnmpVarBind  SnmpVarBind;
+struct SnmpVarBind {
     Object base;
     uint8_t tag;        // 🚀 타입 태그 필드 추가
     char oid[256];
@@ -12,15 +13,13 @@ typedef struct SnmpVarBind {
     const char* (*getTypeName) (SnmpVarBind* self);
     int         (*asInt)       (SnmpVarBind* self);
     long long   (*asLong)      (SnmpVarBind* self);
-} SnmpVarBind;
+};
 
 extern const Class SnmpVarBind_Class;
 
 // 🚀 tag 인자 추가된 생성자
 SnmpVarBind* new_SnmpVarBind(uint8_t tag, const char* oid, const char* value);
-
 bool snmp_asn_decode_response(const uint8_t* buf, size_t len, ArrayList* out_varbinds);
-
 size_t snmp_asn_encode_pdu(uint8_t* buf, size_t buf_sz, uint8_t pdu_type,
                            int version_val, const char* sec_name,
                            const char* oid, int non_repeaters, int max_repetitions,
