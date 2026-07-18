194189ef8c4ed2871dfe9eb09ee2ba838aae7c53
toursmurf <idong322@naver.com>
2026-07-18 17:33:15 +0900

    데드코드 제거 완료

--- include/http_transport.h
+++ include/http_transport.h
@@ -3,6 +3,7 @@
 
 #include "socket_base.h"
 #include <sys/types.h>
+#include <stdbool.h>
 
 typedef struct {
     Socket* sock;
@@ -10,17 +11,22 @@ typedef struct {
     char* path;
     int port;
     char scheme[16];
+    int is_ssl;
 
     char read_buf[8192];
     int read_pos;
     int read_end;
+
+    void* ring;         
+    bool  use_uring;
 } HttpTransport;
 
-HttpTransport* HttpTransport_connect(const char* url);
+HttpTransport* HttpTransport_connect(const char* url, void* ring, int timeout_ms);
 ssize_t HttpTransport_send(HttpTransport* self, const void* buf, size_t len);
 ssize_t HttpTransport_recv(HttpTransport* self, void* buf, size_t len);
 int HttpTransport_getc(HttpTransport* self);
 int HttpTransport_recv_line(HttpTransport* self, char* line_buf, int max_len);
 void HttpTransport_close(HttpTransport* self);
+ssize_t HttpTransport_read(HttpTransport* transport, void* buffer, size_t size);
 
 #endif /* HTTP_TRANSPORT_H */
